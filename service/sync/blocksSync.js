698cbedc8f4d97fe5ac918cf7c5bb181312b1dea
Anish Agnihotri <contact@anishagnihotri.com>
2020-04-09 12:25:49 -0400

    Setting up blok syncing pt. 1

--- service/sync/blocksSync.js
+++ service/sync/blocksSync.js
@@ -2,13 +2,42 @@
 	blockSync.js
 	____________
 	Syncs current round with blocks database.
+	TODO: Error handling
+	TODO: Speed limiting
 */
-var constants = require('../global');
 
-const axios = require('axios');
-const nano = require("nano")(`http://${constants.dbuser}:${constants.dbpass}@${constants.dbhost}`);
+var constants = require('../global'); // Require global constants
+var dbscripts = require('./initSync'); // Require db init scripts
+const axios = require('axios'); // Require axios for requests
+const nano = require("nano")(`http://${constants.dbuser}:${constants.dbpass}@${constants.dbhost}`); // Connect nano to db
 
-const blocks = nano.db.use("blocks");
+dbscripts.initBlocksDB(); // Check if blocks db exists, else, create it.
+let blocks = nano.db.use('blocks'); // Connect to blocks db
+
+async function updateBlocks() {
+	// syncedBlockNumber = head round in db
+	// currentRound = head round from algod
+	let syncedBlockNumber, currentRound;
+	
+	nano.db.get('blocks').then((body) => {
+		syncedBlockNumber = body.doc_count; // Get current db synced block
+	})
+
+	currentRound = await getCurrentRound(); // Get latest round
+
+	setInterval(async() => {
+		currentRound = await getCurrentRound(); // Get new latest round every half second
+	}, 500);
+
+	for (syncedBlockNumber; syncedBlockNumber < currentRound; syncedBlockNumber++) {
+		await addBlock(syncedBlockNumber, currentRound);
+		await sleep(250);
+	}
+
+	setInterval(() => {
+		updateBlocks();
+	}, 1000);
+}
 
 async function getCurrentRound() {
 	let data;
@@ -23,25 +52,6 @@ async function getCurrentRound() {
 	})
 
 	return data;
-}
-
-async function updateBlocks() {
-	let syncedBlockNumber = 0;
-
-	nano.db.get('blocks').then((body) => {
-		syncedBlockNumber = body.doc_count;
-	})
-
-	let currentRound = await getCurrentRound();
-
-	setInterval(async() => {
-		currentRound = await getCurrentRound();
-	}, 500);
-
-	for (syncedBlockNumber; syncedBlockNumber < currentRound; syncedBlockNumber++) {
-		await addBlock(syncedBlockNumber, currentRound);
-		await sleep(250);
-	}
 }
 
 function sleep(ms) {
