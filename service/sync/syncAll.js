96b01a68816aca7a7d4ab5ce326869b542434bcd
Anish Agnihotri <contact@anishagnihotri.com>
2020-04-12 11:26:48 -0400

    Completed, working sync script

--- service/sync/syncAll.js
+++ service/sync/syncAll.js
@@ -10,11 +10,12 @@ const nano = require("nano")(`http://${constants.dbuser}:${constants.dbpass}@${c
 
 let blocks = nano.db.use('blocks'); // Connect to blocks db
 let transactions = nano.db.use('transactions'); // Connect to transactions db
-let addresses = nano.db.use('addresses');
+let addresses = nano.db.use('addresses'); // Connect to addresses db
 
 /*
 	Update blocks in database based on
 	current block in db and highest existing block from blockchain
+	Run in bulk when sync is behind by 250+ blocks
 */
 async function updateBlocks() {
 	// syncedBlockNumber = head round in db
@@ -30,37 +31,41 @@ async function updateBlocks() {
 		currentRound = await getCurrentRound(); // Get new latest round every half second
 	}, 500);
 
-	// FIXME: remove this after testing
-	syncedBlockNumber += 55000;
+	do {
+		// Ignore no-loop-func rule for syncing in do/while
+		// eslint-disable-next-line no-loop-func
+		await bulkAddBlocks(syncedBlockNumber, currentRound).then(async () => {
+			syncedBlockNumber += await 250; // Add 250 blocks, and increment the synced number
+		}).catch(error => {
+			console.log("Error when incrementing syncedBlockNumber during bulk block addition: " + error);
+		});
+	} while (currentRound - 250 > syncedBlockNumber);
+}
 
-	// If there are more than 250 blocks to sync
-	if (syncedBlockNumber + 250 < currentRound) {
-		do {
-			// FIXME: Fix the variable positioning; works for now
-			// eslint-disable-next-line no-loop-func
-			await bulkAddBlocks(syncedBlockNumber, currentRound).then(() => {
-				syncedBlockNumber += 250; // Add 250 blocks, and increment the synced number
-			}).catch(error => {
-				console.log("Error when incrementing syncedBlockNumber during bulk block addition: " + error);
-			});
-		} while (currentRound - 250 > syncedBlockNumber);
+/*
+	Update blocks in database based on
+	current block in db and highest existing block from blockchain
+	Run every 50ms (post-execution time) to keep database in sync with any new data
+*/
+async function keepBlocksUpdated() {
+	// syncedBlockNumber = head round in db
+	// currentRound = head round from algod
+	let syncedBlockNumber, currentRound;
+
+	syncedBlockNumber = await getSyncedRound(); // Get latest synced round from db
+	currentRound = await getCurrentRound(); // Get latest round
+	
+	// If there are new blocks:
+	if (syncedBlockNumber < currentRound) {
+		// Add new blocks to database
+		await addBlock(syncedBlockNumber, currentRound);
 	} else {
-		// If there are less than 250 blocks to sync
-		do {
-			// FIXME: Fix the variable positioning; works for now
-			// eslint-disable-next-line no-loop-func
-			await addBlock(syncedBlockNumber, currentRound).then(() => {
-				syncedBlockNumber++; // Add blocks one-by-one, and increment the synced number
-			}).catch(error => {
-				console.log("Error when incrementing syncedBlockNumber during single block addition: " + error);
-			});
-		} while (syncedBlockNumber < currentRound && syncedBlockNumber + 250 > currentRound);
+		// Else, skip
+		console.log("Blocks are up to date, sleeping for 50ms");
 	}
 
-	// Once syncedBlockNumber === currentRound, run updateBlocks() once every second.
-	setInterval(() => {
-		updateBlocks(); // Self update call
-	}, 1000);
+	// Recursive call self after 50ms sleep
+	await setTimeout(keepBlocksUpdated, 50);
 }
 
 /*
@@ -186,7 +191,7 @@ async function bulkAddBlocks(blockNum, currentNum) {
 }
 
 /*
-	Add block data to CouchDB
+	Add block data to CouchDB (non-bulk, singular)
 */
 async function addBlock(blockNum, currentNum) {
 	await axios({
@@ -195,10 +200,37 @@ async function addBlock(blockNum, currentNum) {
 		headers: {'X-Algo-API-Token': constants.algodapi}
 	}).then(async response => {
 		blocks.insert(response.data); // Insert block data to blocks database as doc
+		let timestamp = response.data.timestamp; // Collect timestamp from block
+
 		if (Object.keys(response.data.txns).length > 0) {
 			let alltransactions = response.data.txns.transactions;
 			for (let i = 0; i < alltransactions.length; i++) {
+				alltransactions[i].timestamp = timestamp; // Add timestamp to transaction (simplify front-end reporting)
 				transactions.insert(alltransactions[i]);
+				
+				// If transaction is of payment type
+				if (typeof alltransactions[i].payment !== 'undefined') {
+					let from_account_id = alltransactions[i].from; // From account
+					let to_account_id = alltransactions[i].payment.to; // To account
+
+					// Add transaction to from account
+					await addresses.get(from_account_id).then(async from_body => {
+						// If account exists in db, update the record
+						await addresses.insert({_id: from_body._id, _rev: from_body._rev, transactions: [alltransactions[i], ...from_body["transactions"]]});
+					}).catch(async () => {
+						// If account does not exist in db, create a new record
+						await addresses.insert({_id: from_account_id, transactions: [alltransactions[i]]});
+					});
+
+					// Add transaction to to account
+					await addresses.get(to_account_id).then(async to_body => {
+						// If account exists in db, update the record
+						await addresses.insert({_id: to_body._id, _rev: to_body._rev, transactions: [alltransactions[i], ...to_body["transactions"]]});
+					}).catch(async () => {
+						// If account does not exist in db, create a new record
+						await addresses.insert({_id: to_account_id, transactions: [alltransactions[i]]});
+					});
+				}
 			}
 		}
 		console.log(`Block added: ${blockNum} of ${currentNum}`); // Log block addition
@@ -207,5 +239,13 @@ async function addBlock(blockNum, currentNum) {
 	})
 }
 
-// Run updateBlocks script
-updateBlocks();
+// Run script
+async function run() {
+	// updateBlocks runs to bring database +/- 250 blocks to blockchain
+	await updateBlocks();
+	// Recursive keepBlocksUpdated runs to keep database in +/- 1 sync to blockchain
+	setTimeout(keepBlocksUpdated, 50);
+}
+
+// Execute syncAll script
+run();
